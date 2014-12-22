698ce81db24c4b575ae819d11846c0d4fa61da6c
Stacey Ell <stacey.ell@gmail.com>
2014-12-22 00:37:56 -0600

    Bunch of changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 extern crate libc;
 
+use std::iter::IteratorExt;
 use std::collections::{hash_map, HashMap};
 use std::str::Chars;
 use std::rand::{task_rng, Rng};
 use std::mem::transmute;
-use std::c_vec::CVec;
+use std::slice;
 use std::slice::bytes::copy_memory;
 use libc::{c_void, c_uchar, c_int, c_uint};
 
-const MARKOV_ORDER: uint = 9;
+const MARKOV_ORDER: uint = 13;
 
 #[deriving(Show, Copy, PartialEq, Eq, Hash)]
 struct MarkovKey([char, ..MARKOV_ORDER]);
@@ -37,7 +38,8 @@ impl AsMarkovIter for str {
     fn as_markov_iter<'a>(&'a self) -> MarkovIter<'a> {
         MarkovIter {
             cur_key: MarkovKey::new(),
-            source: self.chars()
+            source: self.chars(),
+            finished: false,
         }
     }
 }
@@ -45,17 +47,24 @@ impl AsMarkovIter for str {
 struct MarkovIter<'a> {
     cur_key: MarkovKey,
     source: Chars<'a>,
+    finished: bool,
 }
 
 impl<'a> Iterator<(MarkovKey, char)> for MarkovIter<'a> {
     fn next(&mut self) -> Option<(MarkovKey, char)> {
+        if self.finished {
+            return None;
+        }
         match self.source.next() {
             Some(chr) => {
                 let emit_key = self.cur_key;
                 self.cur_key = self.cur_key.next(chr);
                 Some((emit_key, chr))
             },
-            None => None
+            None => {
+                self.finished = true;
+                Some((self.cur_key, '\0'))
+            }
         }
     }
 }
@@ -125,6 +134,20 @@ impl MarkovGenerator {
         self.speak_from_key(MarkovKey::new())
     }
 
+    fn reply(&self, message: &str) -> Result<String, ()> {
+        let keys: Vec<_> = message.as_markov_iter().collect();
+        for (key, _) in keys.into_iter().rev() {
+            println!("Attempt key = {}", key);
+            for _ in range(0u, 10) {
+                return Ok(self.speak_from_key(key));
+                // if 35 <= resp.len() {
+                //     return Ok(resp);
+                // }
+            }
+        }
+        Err(())
+    }
+
     fn speak_from_key(&self, key: MarkovKey) -> String {
         let mut key = key;
         let mut output = String::new();
@@ -143,21 +166,23 @@ impl MarkovGenerator {
             match self.table.get(&key) {
                 Some(value) => {
                     let next_char = value.pick(&mut rng);
-                    if next_char != '\0' {
-                        output.push(next_char);
+                    if next_char == '\0' {
+                        break;
                     }
+                    output.push(next_char);
                     key = key.next(next_char);
                 }
-                None => break
-            }
-
-            // FIXME: we need an end-of-sentence marker.
-            if 4096 < output.len() {
-                break;
+                None => break,
             }
         }
         output
     }    
+}
+
+impl Drop for MarkovGenerator {
+    fn drop(&mut self) {
+        println!("Dropping MarkovGenerator");
+    }
 }
 
 #[no_mangle]
@@ -176,23 +201,60 @@ pub extern "C" fn markov_dealloc(ptr: *mut c_void) -> c_int {
 }
 
 #[no_mangle]
-pub extern "C" fn markov_learn(ptr: *mut c_void, buf: *mut c_uchar, len: c_int) -> c_int {
+pub extern "C" fn markov_reply(
+    ptr: *mut c_void,
+    ibuf: *const c_uchar, ilen: c_int,
+    obuf: *mut c_uchar, olen: c_int,
+) -> c_int {
+    if ptr.is_null() || ibuf.is_null() || obuf.is_null() {
+        return -1;
+    }
+
+    let input_buf = unsafe { slice::from_raw_buf(&ibuf, ilen as uint) };
+    let mut owned_buf = Vec::new();
+    owned_buf.push_all(input_buf.as_slice());
+    let to_learn = match String::from_utf8(owned_buf) {
+        Ok(string) => string,
+        Err(_) => return -2,
+    };
+
+    let gen: Box<MarkovGenerator> = unsafe { transmute(ptr) };
+    let phrase_res = gen.reply(to_learn.as_slice());
+    let _: *mut c_void = unsafe { transmute(gen) };
+
+    let phrase = match phrase_res {
+        Ok(response) => response,
+        Err(()) => return -3,
+    };
+
+    
+    let mut output_buf = unsafe { slice::from_raw_mut_buf(&obuf, olen as uint) };
+    if phrase.as_bytes().len() < output_buf.len() {
+        copy_memory(output_buf.as_mut_slice(), phrase.as_bytes());
+        phrase.as_bytes().len() as c_int
+    } else {
+        -1
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn markov_learn(ptr: *const c_void, buf: *const c_uchar, len: c_int) -> c_int {
     if ptr.is_null() {
         return -1;
     }
-    let mut gen: Box<MarkovGenerator> = unsafe { transmute(ptr) };
-    let input_buf = unsafe { CVec::new(buf, len as uint) };
+
+    let input_buf = unsafe { slice::from_raw_buf(&buf, len as uint) };
 
     let mut owned_buf = Vec::new();
-    owned_buf.push_all(input_buf.as_slice());
+    owned_buf.push_all(input_buf);
 
     let to_learn = match String::from_utf8(owned_buf) {
         Ok(string) => string,
         Err(_) => return -2,
     };
-    gen.learn(to_learn.as_slice());
 
-    // Prevent drop
+    let mut gen: Box<MarkovGenerator> = unsafe { transmute(ptr) };
+    gen.learn(to_learn.as_slice());
     let _: *mut c_void = unsafe { transmute(gen) };
 
     0
@@ -204,11 +266,10 @@ pub extern "C" fn markov_speak(ptr: *mut c_void, buf: *mut c_uchar, len: c_uint)
     if ptr.is_null() {
         return 0;
     }
-    let mut output_buf = unsafe { CVec::new(buf, len as uint) };
+    let mut output_buf = unsafe { slice::from_raw_mut_buf(&buf, len as uint) };
+    
     let gen: Box<MarkovGenerator> = unsafe { transmute(ptr) };
     let phrase = gen.speak();
-
-    // Prevent drop
     let _: *mut c_void = unsafe { transmute(gen) };
 
     if phrase.as_bytes().len() < output_buf.len() {
@@ -217,4 +278,12 @@ pub extern "C" fn markov_speak(ptr: *mut c_void, buf: *mut c_uchar, len: c_uint)
     } else {
         0
     }
+}
+
+
+#[test]
+fn test_empty_gen() {
+    let gen = MarkovGenerator::new();
+    assert!(gen.reply("").is_err());
+    panic!();
 }
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import os
 import ctypes
+import codecs
 
 
 def get_lib_filename(base_dir, name):
     for fn in os.listdir(base_dir):
+        if fn == "lib{}.so".format(name):
+            return os.path.join(base_dir, fn)
         if fn.startswith('lib{}-'.format(name)) and fn.endswith('.so'):
             return os.path.join(base_dir, fn)
     else:
@@ -16,6 +19,13 @@ libmarkov.markov_alloc.restype = ctypes.c_void_p
 libmarkov.markov_dealloc.argtypes = [ctypes.c_void_p]
 libmarkov.markov_dealloc.restype = ctypes.c_int
 
+libmarkov.markov_reply.argtypes = [
+    ctypes.c_void_p,
+    ctypes.c_char_p, ctypes.c_uint,
+    ctypes.c_char_p, ctypes.c_uint
+]
+libmarkov.markov_reply.restype = ctypes.c_int
+
 libmarkov.markov_learn.argtypes = [
     ctypes.c_void_p, ctypes.c_char_p, ctypes.c_uint]
 libmarkov.markov_learn.restype = ctypes.c_int
@@ -27,16 +37,29 @@ libmarkov.markov_speak.restype = ctypes.c_int
 
 class MarkovGenerator(object):
     def __init__(self):
-        self._ptr = libmarkov.markov_alloc()
+        self._lib = libmarkov
+        self._ptr = self._lib.markov_alloc()
         if not self._ptr:
             raise Exception("allocation failure")
 
     def __del__(self):
         if self._ptr is not None:
-            rv = libmarkov.markov_dealloc(self._ptr)
+            rv = self._lib.markov_dealloc(self._ptr)
             if rv != 0:
                 raise Exception("errno = {}".format(-rv))
-        self._ptr = None
+            self._ptr = None
+
+    def reply(self, message):
+        buf_type = ctypes.c_char * len(message)
+        ibuf = buf_type(*list(message))
+        obuf = ctypes.create_string_buffer(8192)
+        rv = libmarkov.markov_reply(
+            self._ptr,
+            ibuf, ctypes.sizeof(ibuf),
+            obuf, ctypes.sizeof(obuf))
+        if rv < 0:
+            raise Exception("errno = {}".format(-rv))
+        return codecs.decode(obuf.raw[0:rv], 'utf8')
 
     def learn(self, message):
         buf_type = ctypes.c_char * len(message)
@@ -50,5 +73,30 @@ class MarkovGenerator(object):
         rv = libmarkov.markov_speak(self._ptr, buffer_, ctypes.sizeof(buffer_))
         if rv < 0:
             raise Exception("errno = {}".format(-rv))
-        return buffer_.raw[0:rv]
+        return codecs.decode(buffer_.raw[0:rv], 'utf8')
+
+
+test_data = """Too many cooks spoil the soup.  Or in this case, two of the cooks went off to work at a fast food place, leaving one cook behind to try and finish the soup
+I know you're being sarcastic, but there are people who reply like that seriously. They actually think they only reason western animation died and chinks thrived is because of wages. There were great western guys before. But, now you've got kids in overpriced west coast art schools who think they're hot shit, when they'd really just get steamrolled by the simplest in-between job for studio DEEN.
+So what's so terrible about streaming anime, /a/?
+Currently living on campus at my uni and theyve been known crack down to crack down hard on any piracy they see. I could get my own router and ISP if I really wanted but I think I can deal with it for a semester
+We (and basically everyone else) only care about unauthorized filesharing if we get a DMCA notice because of it (since we're basically an ISP they get sent to us and we're supposed to forward them once we've identified the person who did it). We don't sit and stare at the squid logs or something looking for bittorrent packet headers or something equally autistic.  Also relevant: a while ago, I did some analysis of >10k DMCA notices received over the past several years. As far as I can tell, there have been zero notices related to anime in any way. You're perfectly safe.
+OP here. I have tried torrents but I don't understand how to use them, yeah. But I also see nothing wrong with streaming HD on a good laptop with good WiFi. No buffering and regardless of whether or not downloads look slightly better, it's still good quality.
+Bitch was scary. What kind of child has absolutely no desires at all? I bet all Ruuko did all day was stare at the wall. When she was told to do homework, she did. When she was told to play outside, she did. She didn't have any foods she liked or disliked, she just ate everything and said it was good when asked. That's not normal.
+Weak season, good ending. Nice to see everyone as humans, was disappointed they didn't go into Ruuko and her lack of any desire more, but that's more of an issue with the series overall after they gave her the I want to free everyone wish.
+Second part of my post, her desire stemmed out of wanting to save everyone, or I guess that kick-started it. I was hoping for something different than the usual save em all mentality from the protagonist. That just felt off for me, but I guess I can accept that she wanted to save everyone because she couldn't stand, as someone with no desires, seeing others with wishes being destroyed.
+I don't have any problems to say I kind of enjoy Okada's way to handle their characters. When they are not acting like fucking retards. Right now, I'm watching True Tears and I'm watching characters acting like humans.
+I feel like Akira's conclusion was kind of shitty. Like, she's suddenly just sane and working in the modeling industry again? You don't usually recover from this kind of insanity.
+"""
+
+def get_populated_mg():
+    mg = MarkovGenerator()
+    for line in test_data.split("\n"):
+        mg.learn(line)
+    return mg
+
+if __name__ == '__main__':
+    mg = get_populated_mg()
+    for _ in xrange(10):
+        print("{}".format(mg.speak()))
 
